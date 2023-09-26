4934c88e635298acdbbd2a6f9854ccb18e4c6a21
Clembs <clembs@clembs.com>
2023-09-26 15:59:00 +0200

    feat: cleanup gigogne code

--- TP4/gigogne.adb
+++ TP4/gigogne.adb
@@ -1,18 +1,11 @@
 with Traceur; use Traceur;
 
 procedure gigogne is
-  ecart    : Integer := 0;
   nbUnites : Integer := 20;
-  --  direction : Integer := 0;
 begin
-  -- initialise le traceur
   initialiserTraceur;
-
-  -- se positionner au centre sans tracer
   leverStylet;
   centrerStylet;
-
-  -- orienter le stylet vers le nord
   orienterNord;
 
   while nbUnites <= 20 + (50 * 4) loop
@@ -36,17 +29,12 @@ begin
       for I in 0 .. nbUnites loop
         deplacerStylet;
       end loop;
-      --  centrerStylet;
       pivoterDroite;
     end loop;
 
     nbUnites := nbUnites + 50;
   end loop;
 
-  -- revenir au centre sans tracer
   leverStylet;
-  --  centrerStylet;
-
-  -- affiche le dessin
   afficherTraceur;
 end gigogne;
